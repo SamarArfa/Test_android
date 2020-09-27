ca511fa46d715f91acdd20d0315fb95bb8d153a8
SamarArfa <SamarArfa1998@gmail.com>
2020-09-27 16:08:49 +0200

    code style in android

---
+++ lib/src/main/java/com/example/CodeStyle.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class CodeStyle {
+    public static void main(String[] args){
+
+    }
+  public  void Empty(String aString,
+                   int  aList,
+                   String aMap,
+                   int anInt, long aLong,
+                   int aSet,
+                   double aDouble) {
+
+        if (aSet == anInt)System.out.println("Spoon!");
+    }
+
+}
